170678daa399a2c836ea4ec8d6fab6c4f2e4578b
Victor Foreman <vbforeman@gmail.com>
2014-06-18 23:38:13 -0400

    created schema and utilities libaries

--- Mongo-Manager.js
+++ Mongo-Manager.js
@@ -7,29 +7,39 @@ rjs.config({
 		mySchemas:'lib/mySchemas'
 	}
 });
-rjs(['mongoose','jquery','underscore','utilities','mySchemas'],function(mongoose,$,_,Helper,mySchemas){
-	return(function(){
-					'use strict';
-		var ret = {}; 
-		console.log(mySchemas);
+rjs(['mongoose','jquery','underscore','utilities','mySchemas'],function(mongoose,$,_,H,mySchemas){
+
+		'use strict';console.log('Mongo-Manager');
+var ret = {}; //object everything will be returned in
 /*
 STATIC DATA AND METHODS
 */
 var data = {other:undefined,staticData:undefined,instanceData:undefined,defaultData:undefined};
+H.inspect('Mongo-Manager.data',data,null,4);
 var attr = [];
 var func = [];
 var events = [{'connection':[]}];
 var fns={staticData:'func',instanceData:'func',defaultData:'func'};
+var dflt ={
+		localhost: '127.0.0.1',
+		dbname: 'devmongomanager',
+		port: 27017,
+		access:'user'
+}
+console.log(dflt);
+var Schema = mongoose.Schema;
 
-
-	var localhost = '127.0.0.1',
-		dbname = 'devmongomanager',
-		port = 27017;
-	var Schema = mongoose.Schema;
-	// mongoose.connection.on('error')
+var init = function(){	console.log('init function')
+	
+}
 /*
 CONNECT
 */
+var connect = function(optIn){console.log('connect function')
+	dflt = _.defaults(optIn,dflt);
+	
+	console.log(dflt);
+};
 
   
 /*
@@ -39,6 +49,11 @@ var sessionSchema = Schema({
 	timeStamp:{}
 });
 
-})();
+ret.connect = connect;
+ret.init = init;
+H.inspect('Mongo-Manager',ret);
+
+
+
 
 });
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 var mgm = require('./Mongo-Manager');
-  
+var util = require('util');
+  console.log(util.inspect(mgm));
+  mgm;
